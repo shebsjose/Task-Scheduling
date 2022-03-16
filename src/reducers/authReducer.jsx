983d0f6dd24790fbd2b61a43c237d0654cae6a17
Sheba Jose <shjose@bestpeers>
2022-03-16 17:33:53 +0530

    Add reducer functionality

--- src/reducers/authReducer.jsx
+++ src/reducers/authReducer.jsx
@@ -1,7 +1,31 @@
-  const loginReducer = () => {
-      return ( 
-          <div>login Reducer</div>
-       );
+const initialState = {
+  loading: null,
+  users: [],
+  error: "",
+};
+
+const loginReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "FETCH_USER_REQUEST":
+      return {
+        ...state,
+        loading: true,
+      };
+    case "FETCH_USER_SUCCESS":
+      return {
+        loading: false,
+        users: action.payload,
+        error: "",
+      };
+    case "FETCH_USER_FAILURE":
+      return {
+        loading: false,
+        users: [],
+        error: action.payload,
+      };
+    default:
+      return state;
   }
-   
-  export default loginReducer;
+};
+
+export default loginReducer;
