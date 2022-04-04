8c28b684a98f5f01d4663cf4532fdfaa3c05c26d
Sheba Jose <shjose@bestpeers>
2022-04-04 11:16:24 +0530

    Pass the props to CreateModel component

---
+++ src/pages/taskPage.jsx
@@ -0,0 +1,27 @@
+import Card from "../components/card";
+import { useState } from "react";
+import CreateTaskModel from "../components/createTaskModel";
+
+const TaskPage = () => {
+   
+const [open, setOpen] =useState(false)
+
+     const handleClick = () =>{
+        setOpen(!open);
+     }
+     console.log(open);
+  return (
+     <div>
+       <button  className="w-50 mt-6 bg-indigo-600 rounded-lg px-2 py-2 text-lg text-white tracking-wide font-semibold font-sans rounded-full"
+                onClick={handleClick}
+                >
+        <span>Create Task</span>
+      </button>
+       <Card />
+      {open && <CreateTaskModel setOpen={setOpen} open={open}/>}
+     </div>
+   
+  );
+};
+
+export default TaskPage;
