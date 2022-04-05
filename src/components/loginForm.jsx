4c071e43d441c743c5e75d7f982287005f943ea5
Sheba Jose <shjose@bestpeers.com>
2022-04-05 11:22:38 +0530

    Changes in the button color

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -63,7 +63,7 @@ const LoginForm = () => {
                 onChange={handleChange}
               />
               <button
-                className="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
+                className="transition duration-200 bg-indigo-600 hover:bg-indigo-600 focus:bg-indigo-700 focus:shadow-sm focus:ring-4 focus:ring-indigo-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
                 type="submit"
               >
                 <span className="inline-block mr-2">Login</span>
