ec806fc86117961fb515175e457b1ba5f887b128
Sheba Jose <shjose@bestpeers.com>
2022-04-05 11:19:34 +0530

    Import the task reducer into Root Reducer

--- src/reducers/allReducer.js
+++ src/reducers/allReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import loginReducer from "./authReducer";
+import taskReducer from "./taskReducer";
 
 const allReducer = combineReducers({
   login: loginReducer,
+  task: taskReducer
 });
 
 export default allReducer;
