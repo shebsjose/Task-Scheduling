c67b8af6346cc8a567036c8c031aebe1cf730e80
Sheba Jose <shjose@bestpeers.com>
2022-04-08 19:16:32 +0530

    Show the user inthe select option

--- src/components/listBox.jsx
+++ src/components/listBox.jsx
@@ -1,45 +1,44 @@
-
 import { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { getAllUser } from "../api/authApi";
 
-const ListBox = () => {
-  const options = [
-    { label: 'Fruit', value: 'fruit' },
-    { label: 'Vegetable', value: 'vegetable' },
-    { label: 'Meat', value: 'meat' },
-  ];
+const ListBox = ({users, setTextValues}) => {
 
-  const [value, setValue] = useState('');
-  const task = useSelector(state => state.task.task);
-  console.log(task);
-  const dispatch = useDispatch();
-
-  const handleChange =  async (event) => {
-    await dispatch(getAllUser())
-    setValue(event.target.value);
+  const [select, setSelect] = useState('');
+  
+  const handleChange =  (event) => {
+    console.log(select);
+    setTextValues({...textValues.selectedUser,event.target.value});
   };
 
   return (
     <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-    <div className="relative">
-    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-      Select User
-    </label>
-    <select 
-        className="block appearance-none w-max bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-        value={value} onChange={handleChange}>
-          {options.map((option,index) => (
-            <option key={index} value={option.value}>{option.label}</option>
+      <div className="relative">
+        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+          Select User
+        </label>
+        <select
+          className="block appearance-none w-max bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          value={select}
+          onChange={handleChange}
+        >
+          {users.map((values, index) => (
+            <option key={index}  value={values.userName}>
+              {values.userName}
+            </option>
+        
           ))}
         </select>
-      <div className="mt-6 pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-        <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+        <div className="mt-6 pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+          <svg
+            className="fill-current h-4 w-4"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+          </svg>
+        </div>
       </div>
     </div>
-    </div>
   );
-}
- 
+};
+
 export default ListBox;
- 
