0b80100fcce64bb526145f287ae48effbdcfc025
Sheba Jose <shjose@bestpeers.com>
2022-04-08 19:13:46 +0530

    Call the user Api

---
+++ src/api/usersApi.js
@@ -0,0 +1,19 @@
+
+import axios from "axios";
+import { fetchUserSuccess } from "../actions/authAction";
+import { fetchUserTaskFailure, fetchUserTaskRequest, fetchUserTaskSuccess } from "../actions/usersAction";
+const apiEndPoint = "http://localhost:5000/api/auth";
+
+
+export const getAllUser = () => {
+    return async (dispatch) => {
+      try {
+        dispatch(fetchUserTaskRequest());
+        const { data } = await axios.get(apiEndPoint + "/get-user");
+        dispatch(fetchUserTaskSuccess(data));
+      } catch (error) {
+        console.log(error);
+        dispatch(fetchUserTaskFailure(error.message || "Unexpected Error!!!"));
+      }
+    };
+  };
