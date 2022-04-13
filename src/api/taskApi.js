00aa4a6f526b3cfc62b22e232b375da3880ed7b8
Sheba Jose <shjose@bestpeers.com>
2022-04-13 19:14:21 +0530

    Import toastOptio

--- src/api/taskApi.js
+++ src/api/taskApi.js
@@ -1,14 +1,18 @@
 import axios from "axios";
 import { fetchTaskRequest, fetchTaskSuccess, fetchTaskFailure } from "../actions/taskAction";
+import { toast } from "react-toastify";
+import { toastOptions } from "../utils/utils";
 const apiEndPoint = "http://localhost:5000/api/task";
 
 export const createTask = (textValues) => {
   return async (dispatch) => {
     try {
       await axios.post(apiEndPoint + "/create", textValues);
+      toast.success('Successfully Created', toastOptions);
       return {res : true}
     } catch (error) {
       console.log(error);
+      toast.error(error.response.data, toastOptions);
     }
   };
 };
@@ -21,15 +25,17 @@ export const getAllTask = () => {
     } catch (error) {
       console.log(error);
       dispatch(fetchTaskFailure(error.message || "Unexpected Error!!!"));
+      toast.error(error.response.data, toastOptions);
     }
   };
 };
 
-export const changeTaskStatus = () => {
+export const changeTaskStatus = (task) => {
+  console.log(task);
   return async (dispatch) => {
     try {
       dispatch(fetchTaskRequest());
-      const { data } = await axios.patch(apiEndPoint + "/change-status");
+      const { data } = await axios.patch(apiEndPoint + "/change-status/" + task._id, task);
       dispatch(fetchTaskSuccess(data));
     } catch (error) {
       console.log(error);
