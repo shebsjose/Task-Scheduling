0fd9c4498a1d66ebdf9921f5713060ae5538bef9
Sheba Jose <shjose@bestpeers.com>
2022-04-13 19:11:26 +0530

    Remove toast object

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 import {fetchUserRequest, fetchUserSuccess, fetchUserFailure } from "../actions/authAction";
-
+import { toastOptions } from "../utils/utils";
 
 const apiEndPoint = "http://localhost:5000/api/auth";
 
@@ -9,27 +9,11 @@ export const registerUser = (data) => {
     return async () => {
       try {
         await axios.post(apiEndPoint + "/register", data);
-        toast.success('Successfully Register', {
-          position: "top-center",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          });
-        return { register: true} 
+        toast.success('Successfully Register', toastOptions);
+        return true
       } catch (error) {
-        toast.error(error.response.data, {
-          position: "top-center",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          });
-        return { register: false}
+        toast.error(error.response.data, toastOptions);
+        return false
       }
     };
   };
@@ -42,28 +26,12 @@ export const registerUser = (data) => {
         const { token, user} = data;
         localStorage.setItem('Token', token)
         dispatch(fetchUserSuccess(user));
-        toast.success('Successfully Login', {
-          position: "top-center",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-        });
-        return { login: true}
+        toast.success('Successfully Login',toastOptions);
+        return true
       } catch (error) {
         dispatch(fetchUserFailure(error.message || "Unexpected Error!!!"));
-        toast.error(error.response.data, {
-          position: "top-center",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          });
-        return { login: false}
+        toast.error(error.response.data, toastOptions);
+        return false
       }
     };
   };
