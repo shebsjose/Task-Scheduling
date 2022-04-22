25b90dfb9e9b7557af02295361076a4c7124e5c9
Sheba Jose <shjose@bestpeers.com>
2022-04-22 16:29:45 +0530

    add sweet alert library

--- src/api/taskApi.js
+++ src/api/taskApi.js
@@ -1,21 +1,33 @@
 import axios from "axios";
 import { fetchTaskRequest, fetchTaskSuccess, fetchTaskFailure } from "../actions/taskAction";
-import { toast } from "react-toastify";
-import { toastOptions } from "../utils/utils";
+// import { toast } from "react-toastify";
+import Swal from 'sweetalert2'
+// import { toastOptions } from "../utils/utils";
 const apiEndPoint = "http://localhost:5000/api/task";
 
 export const createTask = (textValues) => {
   return async (dispatch) => {
     try {
       await axios.post(apiEndPoint + "/create", textValues);
-      toast.success('Successfully Created', toastOptions);
+      // toast.success('Successfully Created', toastOptions);
+      Swal.fire(
+        'Good job!',
+        'You Created Successfully !',
+        'success'
+      )
       return {res : true}
     } catch (error) {
       console.log(error);
-      toast.error(error.response.data, toastOptions);
+      //  toast.error(error.response.data, toastOptions);
+      Swal.fire(
+        'Oops',
+        error.response.data,
+        'error'
+      )
     }
   };
 };
+
 export const getAllTask = () => {
   return async (dispatch) => {
     try {
@@ -25,7 +37,12 @@ export const getAllTask = () => {
     } catch (error) {
       console.log(error);
       dispatch(fetchTaskFailure(error.message || "Unexpected Error!!!"));
-      toast.error(error.response.data, toastOptions);
+      // toast.error(error.response.data, toastOptions);
+      Swal.fire(
+        'Oops',
+         error.response.data,
+        'error'
+      )
     }
   };
 };
@@ -40,6 +57,11 @@ export const changeTaskStatus = (task) => {
     } catch (error) {
       console.log(error);
       dispatch(fetchTaskFailure(error.message || "Unexpected Error!!!"));
+      Swal.fire(
+        'Oops',
+        error.response.data,
+        'error'
+      )
     }
   };
 };
@@ -49,11 +71,16 @@ export const deleteTask= (task) => {
   return async (dispatch) => {
     try {
       dispatch(fetchTaskRequest());
-      const { data } = await axios.delete(apiEndPoint + "/change-status/" + task._id );
+      const { data } = await axios.delete(apiEndPoint + "/change-status/" + task._id,task);
       dispatch(fetchTaskSuccess(data));
     } catch (error) {
       console.log(error);
       dispatch(fetchTaskFailure(error.message || "Unexpected Error!!!"));
+      Swal.fire(
+        'Oops',
+        error.response.data,
+        'error'
+      )
     }
   };
 };
